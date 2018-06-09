47d3594e7ee3e2cfc0139a77abca1b03bb1eefc3
AhmedBadee <ahmedbadee17@gmail.com>
2018-06-09 02:29:02 +0200

    configured the addFault action to return the add response

--- src/main/java/com/ghamry/laser/Controllers/FaultController.java
+++ src/main/java/com/ghamry/laser/Controllers/FaultController.java
@@ -1,20 +1,25 @@
 package com.ghamry.laser.Controllers;
 
 import com.ghamry.laser.Entities.Fault;
+import com.ghamry.laser.Entities.Laser;
 import com.ghamry.laser.Services.FaultService;
+import com.ghamry.laser.Services.LaserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class FaultController {
 
     private FaultService faultService;
+    private LaserService laserService;
 
     @Autowired
-    public FaultController(FaultService faultService) {
+    public FaultController(FaultService faultService, LaserService laserService) {
         this.faultService = faultService;
+        this.laserService = laserService;
     }
 
     @RequestMapping("/lasers/faults")
@@ -29,19 +34,20 @@ public class FaultController {
 
     @RequestMapping("/lasers/{laserId}/faults/{id}")    // Only maps to GET request
     public Fault getFaultById(@PathVariable int id) {
-        faultService.getFaultById(id);
-        return null;
+        return faultService.getFaultById(id);
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/lasers/{laserId}/faults")
-    public void addFault(@RequestBody Fault fault, @PathVariable byte laserId) {
-        fault.setLaserId(laserId);
-        faultService.addFault(fault);
+    public String addFault(@RequestBody Fault fault, @PathVariable int laserId) {
+        Laser laser = laserService.getLaserById(laserId);
+        fault.setLaser(laser);
+        return faultService.addFault(fault);
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/lasers/{laserId}/faults/{id}")
-    public void updateFault(@RequestBody Fault fault, @PathVariable byte laserId) {
-        fault.setLaserId(laserId);
+    public void updateFault(@RequestBody Fault fault, @PathVariable int laserId) {
+        Laser laser = laserService.getLaserById(laserId);
+        fault.setLaser(laser);
         faultService.updateFault(fault);
     }
 
